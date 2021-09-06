22eaa214d970716cef33659e4f80c6675736e0e8
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-09-06 14:58:47 +0100

    Update CalculeController.java

--- src/main/java/com/http/tp/controllers/CalculeController.java
+++ src/main/java/com/http/tp/controllers/CalculeController.java
@@ -1,8 +1,16 @@
 package com.http.tp.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.http.tp.classes.Math;
 @RestController
 public class CalculeController {
-
+@PostMapping("calculemath")
+public String getCalcule(@RequestParam int v1,int v2)
+{
+Math m=new 	Math(v1, v2);
+return m.toString();
+}
 }
